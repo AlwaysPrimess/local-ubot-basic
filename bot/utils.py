75340b966bba4ce28658e0c85c3ca62a6cae5cf4
AlwaysPrimess <sunarsehsri434@gmail.com>
2025-10-20 18:41:31 +0700

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,13 @@
 import json
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'database.json')
-OWNER_ID = 6976551745  # ID Owner
+DB_PATH = os.path.join(os.path.dirname(__file__), "database.json")
+
+if not os.path.exists(DB_PATH):
+    with open(DB_PATH, 'w') as f:
+        json.dump({}, f, indent=2)
 
 def load_db():
-    if not os.path.exists(DB_PATH):
-        save_db({})
     with open(DB_PATH, 'r') as f:
         return json.load(f)
 
@@ -17,22 +18,16 @@ def save_db(data):
 def add_user(user_id):
     db = load_db()
     if str(user_id) not in db:
-        db[str(user_id)] = {
-            "status": "free",
-            "api_id": None,
-            "api_hash": None,
-            "session": None
-        }
+        db[str(user_id)] = {"api_id": None, "api_hash": None, "phone": None, "session": None}
         save_db(db)
 
 def set_user_data(user_id, key, value):
     db = load_db()
     if str(user_id) not in db:
         add_user(user_id)
-        db = load_db()
     db[str(user_id)][key] = value
     save_db(db)
 
-def get_user(user_id):
+def get_user_data(user_id):
     db = load_db()
     return db.get(str(user_id), None)
